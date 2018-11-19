77768ca8cf2388f4ecacd7c806d46c73e95c3350
Francis Guimond <francisguimond@gmail.com>
2018-11-18 17:33:12 -0700

    Send first test email

--- main.go
+++ main.go
@@ -1,20 +1,23 @@
 package main
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/sensu/sensu-go/types"
 	"github.com/spf13/cobra"
-	"io/ioutil"
 	"log"
+	"net/smtp"
 	"os"
 )
 
 var (
-	smtpHost  string
-	destEmail string
-	stdin     *os.File
+	smtpHost     string
+	smtpUsername string
+	smtpPassword string
+	smtpPort     uint16
+	destEmail    string
+	fromEmail    string
+	subject      string
+	stdin        *os.File
 )
 
 func main() {
@@ -25,33 +28,71 @@ func main() {
 	}
 
 	cmd.Flags().StringVarP(&smtpHost, "smtpHost", "s", "", "The SMTP host to use to send to send email")
+	cmd.Flags().StringVarP(&smtpUsername, "smtpUsername", "u", "", "The SMTP username")
+	cmd.Flags().StringVarP(&smtpPassword, "smtpPassword", "p", "", "The SMTP password")
+	cmd.Flags().Uint16VarP(&smtpPort, "smtpPort", "P", 587, "The SMTP server port")
 	cmd.Flags().StringVarP(&destEmail, "destEmail", "d", "", "The destination email address")
+	cmd.Flags().StringVarP(&fromEmail, "fromEmail", "f", "", "The from email address")
+	cmd.Flags().StringVarP(&subject, "subject", "S", "", "The email subjetc")
 
 	cmd.Execute()
 }
 
 func run(cmd *cobra.Command, args []string) error {
+	validationError := checkArgs()
+	if validationError != nil {
+		return validationError
+	}
+
+	log.Println("Executing with arguments:", args)
+
+	if stdin == nil {
+		stdin = os.Stdin
+	}
+
+	//eventJSON, err := ioutil.ReadAll(stdin)
+	//log.Println("Event JSON:", eventJSON)
+
+	//event := &types.Event{}
+	//err = json.Unmarshal(eventJSON, event)
+	//if err != nil {
+	//	return fmt.Errorf("failed to unmarshal stdin data: %s", eventJSON)
+	//}
+	//
+	//log.Println("Event", event)
+
+	sendMailError := sendEmail()
+	if sendMailError != nil {
+		return fmt.Errorf("failed to send email: %s", sendMailError)
+	}
+
+	return nil
+}
+
+func checkArgs() error {
 	if len(smtpHost) == 0 {
 		return errors.New("missing smtp host")
 	}
 	if len(destEmail) == 0 {
 		return errors.New("missing destination email address")
 	}
-	log.Println("Executing with arguments: smtpHost", smtpHost, "destEmail", destEmail)
-
-	if stdin == nil {
-		stdin = os.Stdin
+	if len(smtpUsername) == 0 {
+		return errors.New("smtp username is empty")
 	}
-
-	eventJSON, err := ioutil.ReadAll(stdin)
-	log.Println("Event JSON:", eventJSON)
-
-	event := &types.Event{}
-	err = json.Unmarshal(eventJSON, event)
-	if err != nil {
-		return fmt.Errorf("failed to unmarshal stdin data: %s", eventJSON)
+	if len(smtpPassword) == 0 {
+		return errors.New("smtp password is empty")
 	}
-
-	log.Println("Event", event)
+	if len(smtpPassword) == 0 {
+		return errors.New("smtp password is empty")
+	}
+	if len(fromEmail) == 0 {
+		return errors.New("from email is empty")
+	}
 	return nil
+}
+
+func sendEmail() error {
+	body := "Test Email"
+	smtpAddress := fmt.Sprintf("%s:%d", smtpHost, smtpPort)
+	return smtp.SendMail(smtpAddress, smtp.PlainAuth("", smtpUsername, smtpPassword, smtpHost), fromEmail, []string{destEmail}, []byte(body))
 }
