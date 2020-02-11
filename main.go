e4327186f07031ace6f9cb33c3deca4a951ebe9d
Eric Chlebek <eric@sensu.io>
2020-02-11 15:20:08 -0800

    Port sensu-email-handler to sensu-plugin-sdk 0.6
    Signed-off-by: Eric Chlebek <eric@sensu.io>

--- main.go
+++ main.go
@@ -12,8 +12,8 @@ import (
 	"strings"
 	"text/template"
 
+	"github.com/sensu-community/sensu-plugin-sdk/sensu"
 	corev2 "github.com/sensu/sensu-go/api/core/v2"
-	"github.com/sensu/sensu-plugins-go-library/sensu"
 )
 
 type HandlerConfig struct {
@@ -336,11 +336,11 @@ func resolveTemplate(templateValue string, event *corev2.Event) (string, error) 
 	var resolved bytes.Buffer
 	tmpl, err := template.New("test").Parse(templateValue)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 	err = tmpl.Execute(&resolved, *event)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
 	return resolved.String(), nil
